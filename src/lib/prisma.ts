ce24c4253b17332439900a2901eabd946c942b9f
PedroLLOliveira <githubpedrolucas@gmail.com>
2026-05-25 17:11:52 -0300

    fix: adjust init build

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,13 @@
 import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient({
-  datasourceUrl: process.env.DATABASE_URL,
-});
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
